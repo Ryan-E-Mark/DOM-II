0d099e3dc5aebabc02096425a0003191ced88043
Ryan Mark <rymark907@gmail.com>
2021-08-31 14:58:09 -0700

    added wheel event, trying to get select event to work

--- js/index.js
+++ js/index.js
@@ -4,18 +4,24 @@
 const navLinks = document.querySelectorAll('.nav-link');
 // Let's Go text
 const letsGo = document.querySelector('.text-content h2');
-// Paragraph text
-const pText = document.querySelectorAll('p');
+// boatPic
+const boatPic = document.querySelector('.img-fluid');
 // Destination Text
 const destText = document.querySelector('.content-destination p');
 // Destination Img
 const destImg = document.querySelector('.content-destination img');
 // Body Container
 const body = document.querySelector('body');
+// P tags
+const pTags = document.querySelectorAll('p');
+// Intro h2
+const introTxt = document.querySelector('.intro h2');
 
 
 
-// Links Changing Color on Mouseover
+
+
+// Links Changing Color on Mouseover and preventing default
 navLinks.forEach(item => {
     // console.log(item);
     item.addEventListener('mouseover', event => {
@@ -24,17 +30,22 @@ navLinks.forEach(item => {
     item.addEventListener('mouseleave', event => {
         event.target.style.color = '#212529';
     })
+    item.addEventListener('click', event => {
+        event.preventDefault();
+    })
 });
 
 // Lets go text Increases in size on click cancels propagation of body changing color
-letsGo.addEventListener('click', event => {
+letsGo.addEventListener('dblclick', event => {
     event.target.style.fontSize = "5rem";
     event.stopPropagation();
 })
 
-// Boat picture increases in size on scroll down
-document.addEventListener('wheel', event => {
-    event.pText.style.fontSize = "2rem";
+// Boat picture increases in size on click
+boatPic.addEventListener('click', event => {
+    event.target.style.height = '40vh';
+    event.target.style.maxWidth = '110%';
+    event.target.style.width = '110%';
 })
 
 // Changing destination text
@@ -51,8 +62,8 @@ destImg.addEventListener('mouseleave', event => {
     event.target.style.width = '100%';
 })
 
-// Changing background color of body container on click
-body.addEventListener('click', event => {
+// Changing background color of body container on doubleclick
+body.addEventListener('dblclick', event => {
     event.target.style.backgroundColor = '#D6EFC7';
 })
 
@@ -61,4 +72,16 @@ window.addEventListener('keydown', event => {
     if (event.key === 'Escape') {
         body.style.backgroundColor = 'white';
     }
+})
+
+// scaling paragraph tags when resizing window
+pTags.forEach(item => {
+    item.addEventListener('wheel', event => {
+        event.target.style.fontSize = '3rem';
+    });
+})
+
+// Selecting intro h2 changes color
+introTxt.addEventListener('select', event => {
+    event.target.style.backgroundColor = 'green';
 })
