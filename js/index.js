428bcaba04ead1f660f86df3d9bc4aa18025d78f
Ryan Mark <rymark907@gmail.com>
2021-08-31 11:17:07 -0700

    Changed the nav links color on mouseover and mouseoff, on click, the lets go text increases in font size.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// Grabbing Elements 
+
+// Navigation links
+const navLinks = document.querySelectorAll('.nav-link');
+// Let's Go text
+const letsGo = document.querySelector('.text-content h2');
+
+
+// Links Changing Color on Mouseover
+navLinks.forEach(item => {
+    // console.log(item);
+    item.addEventListener('mouseover', event => {
+        event.target.style.color = 'green';
+    });
+    item.addEventListener('mouseleave', event => {
+        event.target.style.color = '#212529';
+    })
+});
+
+// Lets go text Increases in size on click
+letsGo.addEventListener('click', event => {
+    event.target.style.fontSize = "5rem";
+})
